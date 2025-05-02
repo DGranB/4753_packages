6d0d62cdfaaf1690ef2223cf1a83903fc152a00e
DGranB <sherlock0313@cox.net>
2025-05-01 23:52:39 -0500

    fixed check error

--- R/mymlnorm.R
+++ R/mymlnorm.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #' @examples
-#' mymlnorm(x = c(10, 12, 13, 15, 12, 11, 10), mu = seq(10, 15, length = 1000), sig = seq(0.1, 4, length = 1000), lwd = 2, labcex = 1)
+#' mymlnormFunc(x = c(10, 12, 13, 15, 12, 11, 10), mu = seq(10, 15, length = 1000), sig = seq(0.1, 4, length = 1000), lwd = 2, labcex = 1)
 mymlnormFunc = function(x, mu, sig,...){  #x sample vector
   nmu = length(mu) # number of values in mu
   nsig = length(sig)
